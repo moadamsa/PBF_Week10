0581aa429320c3487c287b2b339b4877723338e5
MOCHAMMAD ADAM'S ARZAQI <81493782+moadamsa@users.noreply.github.com>
2021-04-22 00:09:34 +0700

    Belum lengkap commitnya

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: "AIzaSyB9hIWmOqyOPxeYH3Gobazfiw5LyAFVpn8",
+authDomain: "fir-adam-9faca.firebaseapp.com",
+projectId: "fir-adam-9faca",
+storageBucket: "fir-adam-9faca.appspot.com",
+messagingSenderId: "738115112160",
+appId: "1:738115112160:web:5405ec78f361dcba311485",
+measurementId: "G-LN7C48MMRS"
+
+};
